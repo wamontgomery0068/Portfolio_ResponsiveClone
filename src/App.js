c8e8bb2d4bc18d80bd36013a7fdefceb56c4f0ab
Your Name <wamontgomery0068@gmail.com>
2019-01-08 16:01:52 -0600

    Dropdown is complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    sideNav: false,
+    links: ['SERVICES','PORTFOLIO','ABOUT','TEAM','CONTACT']
+  };
   render() {
+    const { sideNav, links } = this.state;
+
+    const linkMap = links.map((e, i) => {
+      return (
+        <div key = {i}>
+          <p> {e} </p>
+        </div>
+      );
+    });
+
     return (
       <div className = "App">
         <div className = "Nav">
@@ -15,11 +29,14 @@ class App extends Component {
             <p className = "Links_Text"> ABOUT </p>
             <p className = "Links_Text"> TEAM </p>
             <p className = "Links_Text"> CONTACT </p>
-            <div className = "Hamburger">
+            <div className = "Hamburger" onClick={()=> this.setState({ sideNav: !sideNav })}>
                 <p className = "Menu_Text"> Menu </p>
                 <img src="http://thsna.org/online/mobile/images/white-menu-icon.png"height="42" width="42"  />
             </div>
           </div>
+        </div>
+        <div className = "Hamburger_Links">
+          {sideNav && <div className="sideNav">{linkMap}</div>}
         </div>
         <div className = "Header">
           <div className = "Top_Header">
@@ -36,5 +53,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
