feaebe900f1ec10f2634ec152a8f2bea5e453111
Your Name <wamontgomery0068@gmail.com>
2019-01-07 18:55:20 -0600

    Finished the Clone. Time to work on Media Query.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "App">
+        <div className = "Nav">
+          <div className = "Logo_Nav">
+            <p className = "Logo_Text"> Start BootStrap </p>
+          </div>
+          <div className = "Links_Nav">
+            <p className = "Links_Text"> SERVICES </p>
+            <p className = "Links_Text"> PORTFOLIO </p>
+            <p className = "Links_Text"> ABOUT </p>
+            <p className = "Links_Text"> TEAM </p>
+            <p className = "Links_Text"> CONTACT </p>
+          </div>
+        </div>
+        <div className = "Header">
+          <div className = "Top_Header">
+            <p className = "Top_Text"> Welcome To Our Studio! </p>
+          </div>
+          <div className = "Middle_Header">
+            <p className = "Middle_Text"> IT'S NICE TO MEET YOU </p>
+          </div>
+          <div className = "Bottom_Header">
+            <p className = "Bottom_Text"> TELL ME MORE </p>
+          </div>
+        </div>
       </div>
     );
   }
